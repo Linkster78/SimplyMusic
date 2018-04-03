047e7dd8135d8b4100096d4285f7a07f09fbf8c3
Link <xxredstonetekxx@gmail.com>
2018-04-03 18:09:41 -0400

    1.4 R1 (Lang Fix to return UNKNOWN instead of null in unknown translations)

--- src/main/java/com/tek/sm/util/lang/Lang.java
+++ src/main/java/com/tek/sm/util/lang/Lang.java
@@ -11,11 +11,10 @@ public class Lang {
 	
 	private static HashMap<String, String> translations = new HashMap<String, String>();
 	private static YamlConfiguration langConfiguration;
+	public static final String UNKNOWN = "Unknown translation";
 	
 	public static void registerTranslation(String id, String defaultTranslation) {
-		if(defaultTranslation == null) {
-			new TranslationException().printStackTrace();
-		}else {
+		if(defaultTranslation != null) {
 			if(langConfiguration == null) translations.put(id, ChatColor.translateAlternateColorCodes('&', defaultTranslation));
 			String langTranslation = langConfiguration.getString(id);
 			translations.put(id, langTranslation == null ? ChatColor.translateAlternateColorCodes('&', defaultTranslation) : ChatColor.translateAlternateColorCodes('&', langTranslation));
@@ -24,8 +23,7 @@ public class Lang {
 	
 	public static String translate(String id) {
 		if(!translations.containsKey(id)) {
-			new TranslationException().printStackTrace();
-			return null;
+			return UNKNOWN;
 		}else {
 			return translations.get(id);
 		}
